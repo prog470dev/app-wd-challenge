557aa001a6b50cc7c8d549bbe8aaaf9852000ae4
prog470dev <tshimizudev4@gmail.com>
2018-05-21 23:29:56 +0900

    プロジェクトの雛形

---
+++ app-wd-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-wd-challenge
+//
+//  Created by 清水大樹 on 2018/05/21.
+//  Copyright © 2018 prog470dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
